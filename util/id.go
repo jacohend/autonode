6eba86d5061dc63a2eab76385c58f02405158d48
jacohend <jacob@tierion.com>
2022-12-16 03:46:43 -0500

    fix ids

--- util/id.go
+++ util/id.go
@@ -3,7 +3,6 @@ package util
 import "github.com/oklog/ulid/v2"
 
 func BytesToUlid(idbytes []byte) (id ulid.ULID) {
-	id = ulid.Make()
-	id.UnmarshalText(idbytes)
+	id = ulid.MustParse(string(idbytes))
 	return
 }
